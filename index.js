bf546be107921991c1b06245fa50bc9ad0f02b60
Pankaj Bhadane <pankaj-kb>
2021-07-17 21:48:48 +0530

    build api

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
-     if (req.url = "/") {
+const express = require("express");
+const app = express();
+let port = process.env.PORT || 3000;
+app.get("/",(req, res) => {
+     res.send
+     {
           const randomLines = require('random-status-lines');
           const quoteline = randomLines.generate();
           data = quoteline;
           res.end(quoteline);
-     } else {
-          res.writeHead(404, {"content-type": "text/html"});
-          res.end("<h1> 404 Error , Page Not Found<h1>");
      }
 });
-
-server.listen(8000, "127.0.0.1", () => {
-     console.log("listening at port 8000");
+app.listen(port, () => {
+     console.log(`app is running on port http://localhost:${port}`);
 });
