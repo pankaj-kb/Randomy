74a1c53d9b23f135871b597862f841fe6d577212
Pankaj Bhadane <pankaj-kb>
2021-07-20 16:14:40 +0530

    fixed minor issues

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const http = require("http");
 const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
+app.get("/",(req, res) => {
+     res.send
+     {
+          console.log("Site is Running");
+     }
+});
 app.get("/naval",(req, res) => {
      res.send
      {
